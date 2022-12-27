d2e03a1f07771d6a26e53460852f0e8081123f9e
Nikita Polovinkin <poloniki@ya.ru>
2022-12-27 22:59:07 +0100

    Adding comments to functions

--- EtsyParser/shop_analytics.py
+++ EtsyParser/shop_analytics.py
@@ -15,57 +15,86 @@ async def basic_shop(shop_id):
     uri = f'shops/{shop_id}'
     return uri
 
-async def basic_shop_info(shop_id):
+import datetime
 
+async def basic_shop_info(shop_id: str) -> pd.DataFrame:
+    # fetch the basic shop information
     uri = await basic_shop(shop_id)
+    # get the response from the URI
     response = await get(uri)
-    date = datetime.utcfromtimestamp(int(response['update_date'])).strftime('%Y-%m-%d %H:%M:%S')
-    creation_date = datetime.utcfromtimestamp(int(response['create_date'])).strftime('%Y-%m-%d %H:%M:%S')
+
+    # convert the update and creation timestamps to datetime objects
+    update_timestamp = int(response['update_date'])
+    update_date = datetime.utcfromtimestamp(update_timestamp).strftime('%Y-%m-%d %H:%M:%S')
+    creation_timestamp = int(response['create_date'])
+    creation_date = datetime.utcfromtimestamp(creation_timestamp).strftime('%Y-%m-%d %H:%M:%S')
+
+    # extract the active listings, sold items, review count, and review average from the response
     active_listings = response['listing_active_count']
     sold_items = response['transaction_sold_count']
     review_count = response['review_count']
-    review_average = round(response['review_average'],2)
+    review_average = round(response['review_average'], 2)
 
-    answer = pd.DataFrame([{'Creation date':creation_date,
-                           'Date of las listing':date,
-                           'Active listings':active_listings,
-                           'Sold items':sold_items,
-                           'Items p/listing':int(round(sold_items/active_listings,0)),
-                           'Reviews':review_count,
-                           'AVG review':review_average}]).T
+    # calculate the average number of sold items per active listing
+    items_per_listing = int(round(sold_items/active_listings, 0))
+
+    # create a dataframe with the extracted information
+    answer = pd.DataFrame([{'Creation date': creation_date,
+                            'Date of last listing': update_date,
+                            'Active listings': active_listings,
+                            'Sold items': sold_items,
+                            'Items per listing': items_per_listing,
+                            'Reviews': review_count,
+                            'Average review': review_average}]).T
     answer.columns = ['values']
+
     return answer
 
 
-async def stat_of_shop_listings(shop_id, full_shop=False):
-
+async def stat_of_shop_listings(shop_id: str, full_shop: bool = False) -> pd.DataFrame:
+    # fetch the active shop listings
     uri = await active_shop_listing(shop_id)
 
+    # get all listings for the shop
     if full_shop:
+        # get the total number of listings
         response = await getv2(uri, {'limit': 1})
-        pages = math.ceil(response['count'] / 100)
-        all_listings = [await getv2(uri, {'limit':100, 'page': each}) for each in range(1,pages+1)]
+        count = response['count']
+        # calculate the number of pages needed to fetch all listings
+        pages = math.ceil(count / 100)
+        # get all listings in pages of 100
+        all_listings = [await getv2(uri, {'limit':100, 'page': page}) for page in range(1, pages+1)]
     else:
-        all_listings = [await getv2(uri, {'limit': 100, 'page': each}) for each in range(1, 2)]
+        # get the first page of listings
+        all_listings = [await getv2(uri, {'limit': 100, 'page': 1})]
 
+    # extract the results from each page of listings
     all_responses = [listing['results'] for listing in all_listings]
-    all_responses = [every for each in all_responses for every in each]
-    all_title_views = {each['title']:each['views'] for each in all_responses}
-    k = Counter(all_title_views)
+    # flatten the list of results
+    all_responses = [item for sublist in all_responses for item in sublist]
+    # create a dictionary mapping titles to views
+    all_title_views = {response['title']: response['views'] for response in all_responses}
+    # count the occurrences of each title
+    counter = Counter(all_title_views)
+    # get the top 10 most viewed listings
+    top_10 = counter.most_common(10)
+    # extract the titles and views from the top 10 listings
+    titles = [item[0] for item in top_10]
+    views = [item[1] for item in top_10]
+    # create a dictionary mapping titles to URLs
+    all_title_views = {response['title']: response['url'] for response in all_responses if response['title'] in titles}
 
-    top_10 = k.most_common(10)
-    titles = [each[0] for each in top_10]
-    views = [each[1] for each in top_10]
-    all_title_views = {each['title']: each['url'] for each in all_responses if each['title'] in titles}
-
-    df = pd.DataFrame({'Title':titles, 'Purchases':views})
+    # create a dataframe with the title and view count for the top 10 listings
+    df = pd.DataFrame({'Title': titles, 'Purchases': views})
 
     def make_clickable(title):
+        # make the title a clickable link to the listing's URL
         return '<a target="_blank" href="{}">{}</a>'.format(all_title_views[title], title)
+
     df['Title'] = df['Title'].apply(make_clickable)
 
-
     return df
+
 
 
 async def get_shop_id(shop_name):
